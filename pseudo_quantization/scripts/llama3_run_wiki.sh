8bcf453a336b231d0ffc5b9ba7bdb9b45e40d746
huweim <506834968@qq.com>
2024-09-06 11:50:53 +0800

    sync

---
+++ pseudo_quantization/scripts/llama3_run_wiki.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+MODEL_SIZE=${1:-"8"}
+TASKS=${2:-"arc_challenge"}
+SHOTS=${3:-"0"}
+QUANT_MODE=${4:-"ant"}
+ANT_MODE=${5:-"int"}
+GROUP_SIZE=${6:-"-1"}
+QUANT_BIT_WIDTH=${7:-"w4a8k16v16"}
+OPTION=${8:-"quant"}
+DESC=${9:-""}
+
+MODEL=/localssd/wmhu/models/llama-3-${MODEL_SIZE}b-hf
+OUTPUT_NAME=llama-3-${MODEL_SIZE}b
+OUTPUT_DIR=""
+
+if [ "$OPTION" == "load" ]; then
+    OUTPUT_DIR=output/output_giant_load
+    EXTRA_OPTION="--load_quant /localssd/wmhu/models/quant_cache/$OUTPUT_NAME-w4-g$GROUP_SIZE-$QUANT_MODE"
+elif [ "$OPTION" == "dump" ]; then
+    OUTPUT_DIR=output/output_giant_dump
+    mkdir -p quant_cache
+    EXTRA_OPTION="--dump_quant quant_cache/$OUTPUT_NAME-w4-g$GROUP_SIZE-$QUANT_MODE"
+elif [ "$OPTION" == "quant" ]; then
+    OUTPUT_DIR=output/output_llama3
+    EXTRA_OPTION=""
+else
+    echo "Invalid option: $OPTION. Only 'load', 'dump', and 'quant' are supported."
+    exit 1
+fi
+
+mkdir -p $OUTPUT_DIR
+
+python -m awq.entry_wikitext --model_path $MODEL \
+    --tasks $TASKS \
+    --num_fewshot $SHOTS \
+    --quant_bit_width $QUANT_BIT_WIDTH \
+    $EXTRA_OPTION \
+    --q_backend fake \
+    --no_zero_point \
+    --quant_mode $QUANT_MODE \
+    --ant_mode $ANT_MODE \
+    --q_group_size $GROUP_SIZE \
+    | tee $OUTPUT_DIR/${OUTPUT_NAME}_${TASKS}_${QUANT_BIT_WIDTH}_${SHOTS}shots_${QUANT_MODE}_${ANT_MODE}_g${GROUP_SIZE}_${DESC}_$(date +%m%d%H%M).log 2>&1
--- pseudo_quantization/scripts/tmp1.sh
+++ pseudo_quantization/scripts/tmp1.sh
@@ -1,3 +1,15 @@
 CUDA_VISIBLE_DEVICES=0 ./scripts/llama2_run_wiki.sh 7 wikitext 0 giant int 64 w4a8k16v16 load
 
 CUDA_VISIBLE_DEVICES=0 ./scripts/opt_run.sh 6.7 wikitext 0 int int 64 w4a8k16v16 quant
+
+
+CUDA_VISIBLE_DEVICES=0 ./scripts/llama2_run_wiki.sh 7 wikitext 0 mxfp int 32 w4a4k16v16 quant
+CUDA_VISIBLE_DEVICES=0 ./scripts/llama2_run_wiki.sh 7 wikitext 0 mxfp int 32 w4a4k4v4 quant
+
+CUDA_VISIBLE_DEVICES=0 ./scripts/llama3_run_wiki.sh 8 wikitext 0 ant int 32 w16a16k16v16 quant
+
+CUDA_VISIBLE_DEVICES=0 ./scripts/llama3_run_wiki.sh 8 wikitext 0 ant float 32 w4a4k16v16 quant
+CUDA_VISIBLE_DEVICES=0 ./scripts/llama3_run_wiki.sh 8 wikitext 0 ant float 64 w4a4k16v16 quant
+CUDA_VISIBLE_DEVICES=0 ./scripts/llama3_run_wiki.sh 8 wikitext 0 ant float 128 w4a4k16v16 quant
+
+CUDA_VISIBLE_DEVICES=0 ./scripts/llama3_run_wiki.sh 8 wikitext 0 mxfp int 32 w4a4k16v16 quant e2m1

f60421bfaa4f91bc0e14110eeeb6bcddd1c9d9b7
huweim <506834968@qq.com>
2024-10-19 11:59:25 +0800

    remove redun

--- pseudo_quantization/awq/kmeans_kernel/kmeans_cuda.h
+++
@@ -1,3 +0,0 @@
-#include <torch/extension.h>
-
-torch::Tensor kmeans_cuda_forward(torch::Tensor input, torch::Tensor centroids, torch::Tensor labels, torch::Tensor output, torch::Tensor initial_centroids, int group_num, int nums_per_group, int num_cluster) ;

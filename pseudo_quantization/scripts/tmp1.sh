4ebba29bab1f41c24a93b0d22315118fc9a54484
huweim <506834968@qq.com>
2025-07-10 15:12:12 +0800

    remove tmp.sh

--- pseudo_quantization/scripts/tmp1.sh
+++
@@ -1,9 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 ./scripts/llama3_run.sh 8 arc_easy,hellaswag,piqa,winogrande,arc_challenge 0 mxfp float 32 w4a4q16k16v16 w-base-a-base quant e1m2
-# MX+
-CUDA_VISIBLE_DEVICES=0 ./scripts/llama3_run.sh 8 arc_easy,hellaswag,piqa,winogrande,arc_challenge 0 mxfp float 32 w4a4q16k16v16 w-sub_group+1+max-a-sub_group+1+max quant e1m2
-CUDA_VISIBLE_DEVICES=0 ./scripts/llama3_run.sh 8 arc_easy,hellaswag,piqa,winogrande,arc_challenge 0 mxfp float 32 w4a4q16k16v16 w-sub_group+2+max-a-sub_group+2+max quant e1m2
-CUDA_VISIBLE_DEVICES=0 ./scripts/llama3_run.sh 8 arc_easy,hellaswag,piqa,winogrande,arc_challenge 0 mxfp float 32 w4a4q16k16v16 w-sub_group+4+max-a-sub_group+4+max quant e1m2
-# OA
-CUDA_VISIBLE_DEVICES=0 ./scripts/llama3_run.sh 8 arc_easy,hellaswag,piqa,winogrande,arc_challenge 0 mxfp float 32 w4a4q16k16v16 w-sub_group+1+outlier-a-sub_group+1+outlier quant e1m2
-CUDA_VISIBLE_DEVICES=0 ./scripts/llama3_run.sh 8 arc_easy,hellaswag,piqa,winogrande,arc_challenge 0 mxfp float 32 w4a4q16k16v16 w-sub_group+2+outlier-a-sub_group+2+outlier quant e1m2
-CUDA_VISIBLE_DEVICES=0 ./scripts/llama3_run.sh 8 arc_easy,hellaswag,piqa,winogrande,arc_challenge 0 mxfp float 32 w4a4q16k16v16 w-sub_group+4+outlier-a-sub_group+4+outlier quant e1m2
--- pseudo_quantization/scripts/tmp6.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 ./scripts/llama3_run.sh 70 wikitext 0 mxfp float 32 w4a4q16k16v16 w-sub_group+1+max-a-sub_group+1+max quant e0m3
-CUDA_VISIBLE_DEVICES=0,1,2,3 ./scripts/llama3_run.sh 70 wikitext 0 mxfp float 32 w4a4q16k16v16 w-sub_group+1+outlier-a-sub_group+1+outlier quant e0m3
-CUDA_VISIBLE_DEVICES=0,1,2,3 ./scripts/llama3_run.sh 70 wikitext 0 mxfp float 32 w4a4q16k16v16 w-sub_group_adaptive+1+max-a-sub_group+1+max quant e0m3
-CUDA_VISIBLE_DEVICES=0,1,2,3 ./scripts/llama3_run.sh 70 wikitext 0 mxfp float 32 w4a4q16k16v16 w-sub_group_adaptive+1+max-a-sub_group+1+outlier quant e0m3

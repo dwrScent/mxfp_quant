fa4aeb9cb771fd23f1f788455c5aed9628f09ea9
huweim <506834968@qq.com>
2025-07-10 15:09:21 +0800

    remove tmp.sh

--- pseudo_quantization/scripts/tmp3.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1 ./scripts/llama2_run.sh 70 wikitext 0 mxfp float 32 w4a16k16v16 w-base-a-base quant mxfp4-baseline
-CUDA_VISIBLE_DEVICES=0,1 ./scripts/llama2_run.sh 70 wikitext 0 ant float 32 w4a16k16v16 w-base-a-base quant fp4
-CUDA_VISIBLE_DEVICES=0,1 ./scripts/llama2_run.sh 70 wikitext 0 mxfp float 32 w4a16k16v16 w-sub_group-a-base quant mx+

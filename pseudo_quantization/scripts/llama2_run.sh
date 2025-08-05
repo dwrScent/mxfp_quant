5d7574c6f80ecdc439d79d298faedca8d85b5f05
subsir <tiancaizhangdaxian@sjtu.edu.cn>
2025-08-05 12:59:35 +0800

    Add wikitext-mse

--- pseudo_quantization/scripts/llama2_run.sh
+++ pseudo_quantization/scripts/llama2_run.sh
@@ -8,7 +8,9 @@ GROUP_SIZE=${6:-"-1"}
 QUANT_BIT_WIDTH=${7:-"w4a8k16v16"}
 MXFP_MODE=${8:-"w-base-a-base"}
 OPTION=${9:-"quant"}
-DESC=${10:-""}
+TOPK=${10:-"1"}
+EM_BIT=${11:-"2"}
+ES_BIT=${12:-"2"}
 
 # MODEL=/localssd/wmhu/models/llama-${MODEL_SIZE}b-hf-transformers-4.29
 # MODEL=/mnt/nvme0n1/ckpt/llama/llama-2-${MODEL_SIZE}b-hf
@@ -45,4 +47,7 @@ python -m mxq.entry --model_path $MODEL \
     --mxfp $MXFP_MODE \
     --ant_mode $ANT_MODE \
     --q_group_size $GROUP_SIZE \
-    | tee $OUTPUT_DIR/${OUTPUT_NAME}_${TASKS}_${QUANT_BIT_WIDTH}_${SHOTS}shots_${QUANT_MODE}_${ANT_MODE}_g${GROUP_SIZE}_${MXFP_MODE}_${DESC}_$(date +%m%d%H%M).log 2>&1
+    --topk $TOPK \
+    --em_bit $EM_BIT \
+    --es_bit $ES_BIT \
+    | tee $OUTPUT_DIR/${OUTPUT_NAME}_${TASKS}_${QUANT_BIT_WIDTH}_${SHOTS}shots_${QUANT_MODE}_${ANT_MODE}_g${GROUP_SIZE}_${MXFP_MODE}_${TOPK}_${EM_BIT}_${ES_BIT}_$(date +%m%d%H%M).log 2>&1

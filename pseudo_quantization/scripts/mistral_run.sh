5c07a639aa5fab681850d54bcd7f001639f6fdf5
huweim <506834968@qq.com>
2025-08-07 16:06:55 +0800

    update, scripts, mistral

--- pseudo_quantization/scripts/mistral_run.sh
+++ pseudo_quantization/scripts/mistral_run.sh
@@ -20,14 +20,14 @@ OUTPUT_NAME=Mistral-${MODEL_SIZE}B
 OUTPUT_DIR=""
 
 if [ "$OPTION" == "load" ]; then
-    OUTPUT_DIR=output/output_giant_load_$(date +%m%d)
+    OUTPUT_DIR=output/output_mistral_load_$(date +%m%d)
     EXTRA_OPTION="--load_quant /localssd/wmhu/models/quant_cache/$OUTPUT_NAME-w4-g$GROUP_SIZE-$QUANT_MODE"
 elif [ "$OPTION" == "dump" ]; then
-    OUTPUT_DIR=output/output_giant_dump_$(date +%m%d)
+    OUTPUT_DIR=output/output_mistral_dump_$(date +%m%d)
     mkdir -p quant_cache
     EXTRA_OPTION="--dump_quant quant_cache/$OUTPUT_NAME-w4-g$GROUP_SIZE-$QUANT_MODE"
 elif [ "$OPTION" == "quant" ]; then
-    OUTPUT_DIR=output/output_llama3_mxfp_test_$(date +%m%d)
+    OUTPUT_DIR=output/output_mistral_mxfp_test_$(date +%m%d)
     EXTRA_OPTION=""
 else
     echo "Invalid option: $OPTION. Only 'load', 'dump', and 'quant' are supported."

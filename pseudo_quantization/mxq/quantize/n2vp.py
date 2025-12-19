e5bb1d4830bf3f737c0d129ac47e172de7217e5a
Li Xunyi <shawn@LideMacBook-Pro.local>
2025-12-19 21:32:25 +0800

    nves_simple_copy

--- pseudo_quantization/mxq/quantize/n2vp.py
+++ pseudo_quantization/mxq/quantize/n2vp.py
@@ -62,8 +62,8 @@ def cast_to_fp4_em(x: torch.Tensor):
     x_abs = torch.abs(x)
     fp4, fp4_index = quantize_to_grid(x_abs, FP4_E2M1_GRID)
     _, fp6_index = quantize_to_grid(x_abs, FP6_E2M3_GRID)
-    print("previous fp6:")
-    print(FP6_E2M3_GRID.to(x.device)[fp6_index])
+    # print("previous fp6:")
+    # print(FP6_E2M3_GRID.to(x.device)[fp6_index])
     fp6_index.clamp_(min=fp4_index * 4 - 1, max=fp4_index * 4 + 2)
     fp6 = FP6_E2M3_GRID.to(x.device)[fp6_index]
 
@@ -110,6 +110,78 @@ def get_quant_mxem(tensor_value: torch.Tensor, group_size: int):
     return tensor_quant.reshape(org_shape).to(org_dtype)
 
 
+@torch.no_grad()
+def get_quant_nves(tensor_value: torch.Tensor, group_size: int):
+
+    sub_group_size = 4  # extra 2 bit for scale in subgroup
+    assert group_size % sub_group_size == 0
+
+    org_shape = tensor_value.shape
+    org_dtype = tensor_value.dtype
+
+    tensor_value = tensor_value.float()
+
+    if group_size > 0:
+        assert org_shape[-1] % group_size == 0
+        tensor_value = tensor_value.reshape(-1, group_size)
+
+    max_val = tensor_value.abs().amax(dim=1, keepdim=True)
+    # avoid divide a too small value
+    max_val = max_val.clamp(min=1e-8)
+
+    max_quant_val = torch.tensor(FLOAT4_E2M1_MAX, device=tensor_value.device)
+
+    scales = tensor_value.abs().amax(dim=1, keepdim=True) / max_quant_val
+
+    tensor_value = tensor_value.reshape(-1, sub_group_size)
+    # Compute the scaling factor
+    global_scale = scales.max() / FLOAT8_E4M3_MAX
+    scales = (
+        (scales / global_scale)
+        .clamp(min=FLOAT8_E4M3_EPS)
+        .to(torch.float8_e4m3fn)
+        .to(tensor_value.dtype)
+    ) * global_scale
+    exp = torch.floor(torch.log2(scales))
+    # exp = torch.floor(torch.log2(max_val)) - torch.floor(torch.log2(max_quant_val))
+    bias_mse = {}
+    range_ = range(-1, 2)
+    org_scales = scales
+    for bias in range_:
+        # scales = torch.pow(2, exp + bias)
+
+        scales = org_scales * torch.pow(2, torch.tensor(bias, device=tensor_value.device, dtype=tensor_value.dtype))
+        sub_groups_per_group = group_size // sub_group_size
+        scales = scales.expand(-1, sub_groups_per_group).reshape(-1, 1)
+        ratios = torch.tensor(
+            [1.0, 1.25, 1.5, 1.75], dtype=tensor_value.dtype, device=tensor_value.device
+        )
+        x_expanded = tensor_value.unsqueeze(2)
+        scales_expanded = scales.unsqueeze(2)
+
+        cand_scales = scales_expanded * ratios.view(1, 1, -1)
+        cand_qval = cast_to_fp4(x_expanded / cand_scales) * cand_scales
+        mse_per_ratio = (cand_qval - x_expanded).pow(2).mean(dim=1)
+        best_ratio_idx = mse_per_ratio.argmin(dim=1)
+        row_idx = torch.arange(tensor_value.size(0), device=tensor_value.device)
+        best_dqval = cand_qval[row_idx, :, best_ratio_idx]
+        quant_mse_per_subgrp = mse_per_ratio[row_idx, best_ratio_idx]
+        tensor_deq = best_dqval.reshape(-1, group_size)
+        quant_mse_sum = quant_mse_per_subgrp.view(-1, sub_groups_per_group).mean(
+            dim=1, keepdim=True
+        )
+        bias_mse[bias] = (tensor_deq, quant_mse_sum)
+    all_mse = torch.cat([bias_mse[b][1] for b in range_], dim=1)
+    best_bias_idx = all_mse.argmin(dim=1)
+    all_deq = torch.stack([bias_mse[b][0] for b in range_], dim=0)
+    all_deq = all_deq.view(len(range_), -1, group_size)
+    idx_expanded = best_bias_idx.view(1, -1, 1).expand(1, -1, group_size)
+    final_deq = torch.gather(all_deq, dim=0, index=idx_expanded).squeeze(0)
+    tensor_deq = final_deq.reshape(org_shape).to(org_dtype)
+    return tensor_deq
+
+
+@torch.no_grad()
 def get_quant_nvem(tensor_value: torch.Tensor, group_size: int):
 
     sub_group_size = 4  # extra 2 bit for mantissa in subgroup
@@ -136,7 +208,7 @@ def get_quant_nvem(tensor_value: torch.Tensor, group_size: int):
     ) * global_scale
 
     fp4, fp6 = cast_to_fp4_em(tensor_value / scales)
-    print(fp4, "\n", fp6)
+    # print(fp4, "\n", fp6)
 
     tmp = fp4.reshape(-1, sub_group_size)
     outlier_mask = torch.zeros_like(tmp, dtype=tensor_value.dtype).to(
@@ -147,7 +219,6 @@ def get_quant_nvem(tensor_value: torch.Tensor, group_size: int):
     outlier_mask.scatter_(1, indices, 1)
     outlier_group_mask = outlier_mask.reshape(-1, group_size)
     tensor_quant = (fp4 * (1 - outlier_group_mask) + fp6 * outlier_group_mask) * scales
-    print("scales:", scales.reshape(-1))
 
     return tensor_quant.reshape(org_shape).to(org_dtype)
 
@@ -156,5 +227,9 @@ __name__ = "__main__"
 a = torch.tensor([-0.27, 10.26, 6.41, 10.78, 9.25, 45.36, 10.72, 1.26])
 
 res = get_quant_nvem(a, 8)
+
+print(res)
+
+res = get_quant_nves(a, 8)
 
 print(res)

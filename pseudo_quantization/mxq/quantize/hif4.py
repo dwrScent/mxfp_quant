284e296114f7c4568b6c092e3971b1cb693ec6d3
tumeScent <rirexunyi@gmail.com>
2026-01-07 11:59:09 +0800

    trying hifem

--- pseudo_quantization/mxq/quantize/hif4.py
+++ pseudo_quantization/mxq/quantize/hif4.py
@@ -290,7 +290,6 @@ def get_quant_nvem(tensor_value: torch.Tensor, group_size: int):
     return tensor_quant.reshape(org_shape).to(org_dtype)
 
 
-
 @torch.no_grad()
 def get_quant_hif4(tensor_value: torch.Tensor, group_size: int):
 
@@ -311,24 +310,67 @@ def get_quant_hif4(tensor_value: torch.Tensor, group_size: int):
     v_max8 = v_max16.reshape(tensor_value.shape[0], -1, 2).amax(dim=2)
     v_max8 = v_max8.reshape(tensor_value.shape[0], 8)
     v_max = v_max8.amax(dim=1, keepdim=True)
-    print("v_max8:", v_max8)
-    print("v_max16:", v_max16)
     SF = cast_to_E6M2(v_max / LEVEL_2_MAX)
     E1_8 = (v_max8 / SF) >= 4
     E1_8 = E1_8.to(v_max8.dtype)
     E1_8x2 = E1_8.repeat_interleave(2, dim=1)
-    print("E1_8x2:", E1_8x2)
     E1_16 = (v_max16 / SF * 2.0 ** (-E1_8x2)) >= 2
     E1_16 = E1_16.to(v_max16.dtype)
-    print("E1_16:", E1_16)
     DE16 = E1_16 + E1_8x2
     DE64 = DE16.repeat_interleave(4, dim=1)
-    print(DE64)
     in_grp = torch.floor(tensor_value.abs() / (SF * 2.0 ** (DE64 - 2)) + 0.5) * 2.0 ** (-2)
     in_grp[in_grp >= 2.0] = 1.75
+    print(in_grp)
     tensor_quant = sign * in_grp * (SF * 2.0 ** DE64)
 
     return tensor_quant.reshape(org_shape).to(org_dtype)
+
+
+@torch.no_grad()
+def get_quant_hifem(tensor_value: torch.Tensor, group_size: int):
+
+    org_shape = tensor_value.shape
+    org_dtype = tensor_value.dtype
+
+    tensor_value = tensor_value.float()
+
+    assert group_size == 64
+    tensor_value = tensor_value.reshape(-1, group_size)
+
+    sign = torch.sign(tensor_value)
+
+    v_max16 = torch.zeros((tensor_value.shape[0], 16), device=tensor_value.device)
+    v_max8 = torch.zeros((tensor_value.shape[0], 8), device=tensor_value.device)
+    v_max16, indices = tensor_value.abs().reshape(tensor_value.shape[0], -1, 4).max(dim=2)
+    # v_max16 = v_max16.reshape(tensor_value.shape[0], 16)
+    v_max8 = v_max16.reshape(tensor_value.shape[0], -1, 2).amax(dim=2)
+    # v_max8 = v_max8.reshape(tensor_value.shape[0], 8)
+    v_max = v_max8.amax(dim=1, keepdim=True)
+    SF = cast_to_E6M2(v_max / LEVEL_2_MAX)
+    E1_8 = (v_max8 / SF) >= 4
+    E1_8 = E1_8.to(v_max8.dtype)
+    E1_8x2 = E1_8.repeat_interleave(2, dim=1)
+    E1_16 = (v_max16 / SF * 2.0 ** (-E1_8x2)) >= 2
+    E1_16 = E1_16.to(v_max16.dtype)
+    DE16 = E1_16 + E1_8x2
+    DE64 = DE16.repeat_interleave(4, dim=1)
+    in_grp = tensor_value.abs() / (SF * 2.0 ** (DE64)) 
+    e1m2 = torch.floor(in_grp * 2.0 ** 2 + 0.5) * 2.0 ** (-2)
+    e1m4 = torch.floor(in_grp * 2.0 ** 4 + 0.5) * 2.0 ** (-4)
+    outlier_mask = torch.zeros_like(e1m2, dtype=tensor_value.dtype).to(
+        tensor_value.device
+    )
+    e1m2[e1m2 >= 2.0] = 1.75
+    e1m4[e1m4 >= 2.0] = 1.9375
+    outlier_mask = outlier_mask.reshape(-1, 16)
+    outlier_mask.scatter_(1, indices, 1)
+    outlier_mask = outlier_mask.reshape(-1, group_size)
+    in_grp = e1m2 * (1 - outlier_mask) + e1m4 * outlier_mask
+    print(in_grp)
+    tensor_quant = sign * in_grp * (SF * 2.0 ** DE64)
+
+    return tensor_quant.reshape(org_shape).to(org_dtype)
+
 
 __name__ = "__main__"
 
@@ -350,6 +392,11 @@ mse = (res - b).pow(2).mean()
 
 print(res)
 print("MSE HIF4:", mse)
+
+res = get_quant_hifem(b, 64)
+mse_em = (res - b).pow(2).mean()
+print(res)
+print("MSE HIFEM:", mse_em)
 
 # print("MSE NVEM:", mse1)
 #

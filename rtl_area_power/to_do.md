0207bfb1af715e121c7948da23da049ab15d81b4
tumeScent <rirexunyi@gmail.com>
2026-05-07 11:12:08 +0800

    fixed abs_dif resolvation. next step is to further hw simplify quant_engine.

--- rtl_area_power/to_do.md
+++ rtl_area_power/to_do.md
@@ -1,3 +1,5 @@
-- combine two multiplier to one
+- two mutiplier
+    - turn to threshold
+    - combine two multiplier to one
 - reduce cost bitwidth
 - do not use fp32 to calculate err (how to garantee precisian if not using dfp32?)
--- rtl_area_power/vsrc/nvesm2/quant_engine/nvesm2_fp32_abs_diff_pos.v
+++ rtl_area_power/vsrc/nvesm2/quant_engine/nvesm2_fp32_abs_diff_pos.v
@@ -3,13 +3,13 @@ module nvesm2_fp32_abs_diff_pos (
     input  [31:0] b,
     output reg [31:0] z
 );
-    reg [31:0] big;
-    reg [31:0] small;
+    reg [31:0] big_fp32;
+    reg [31:0] lo_fp32;
     reg [7:0]  e_big;
-    reg [7:0]  e_small;
+    reg [7:0]  e_lo;
     reg [23:0] m_big;
-    reg [23:0] m_small;
-    reg [23:0] m_small_shifted;
+    reg [23:0] m_lo;
+    reg [23:0] m_lo_shifted;
     reg [24:0] diff;
     reg signed [10:0] exp_unb;
     reg signed [10:0] exp_out;
@@ -51,7 +51,7 @@ module nvesm2_fp32_abs_diff_pos (
     endfunction
 
     always @(*) begin
-        m_small_shifted = 24'd0;
+        m_lo_shifted = 24'd0;
         diff = 25'd0;
         exp_unb = 11'sd0;
         exp_out = 11'sd0;
@@ -61,29 +61,29 @@ module nvesm2_fp32_abs_diff_pos (
         z = 32'h00000000;
 
         if (a[30:0] >= b[30:0]) begin
-            big = a;
-            small = b;
+            big_fp32 = a;
+            lo_fp32 = b;
         end else begin
-            big = b;
-            small = a;
+            big_fp32 = b;
+            lo_fp32 = a;
         end
 
-        e_big = big[30:23];
-        e_small = small[30:23];
-        m_big = {((e_big != 8'd0) ? 1'b1 : 1'b0), big[22:0]};
-        m_small = {((e_small != 8'd0) ? 1'b1 : 1'b0), small[22:0]};
+        e_big = big_fp32[30:23];
+        e_lo = lo_fp32[30:23];
+        m_big = {((e_big != 8'd0) ? 1'b1 : 1'b0), big_fp32[22:0]};
+        m_lo = {((e_lo != 8'd0) ? 1'b1 : 1'b0), lo_fp32[22:0]};
 
         if (e_big == 8'hff) begin
             z = 32'h7f800000;
-        end else if (big[30:0] == small[30:0]) begin
+        end else if (big_fp32[30:0] == lo_fp32[30:0]) begin
             z = 32'h00000000;
         end else begin
-            if ((e_big - e_small) >= 8'd24)
-                m_small_shifted = 24'd0;
+            if ((e_big - e_lo) >= 8'd24)
+                m_lo_shifted = 24'd0;
             else
-                m_small_shifted = m_small >> (e_big - e_small);
+                m_lo_shifted = m_lo >> (e_big - e_lo);
 
-            diff = {1'b0, m_big} - {1'b0, m_small_shifted};
+            diff = {1'b0, m_big} - {1'b0, m_lo_shifted};
             lead_shift = leading_shift24(diff[23:0]);
 
             exp_unb = (e_big == 8'd0) ? -11'sd126 : ($signed({1'b0, e_big}) - 11'sd127);
